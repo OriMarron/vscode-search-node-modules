c99d7e84c670a69090b2c7833534807f18ff82c6
Ori Marron <orim@wix.com>
2019-05-05 17:22:08 +0300

    search packages

--- extension.js
+++ extension.js
@@ -1,6 +1,10 @@
 const vscode = require('vscode');
 const fs = require('fs');
 const path = require('path');
+const util = require('util');
+
+const exists = util.promisify(fs.exists);
+const readdir = util.promisify(fs.readdir);
 
 var lastFolder = '';
 var lastWorkspaceName = '';
@@ -75,6 +79,64 @@ exports.activate = context => {
             });
         };
 
+        const getPackageFolder = async (workspaceFolder) => {
+            const packagesPath = path.join(workspaceFolder.uri.fsPath, 'packages');
+            if (await exists(packagesPath)) {
+                const packages = [];
+
+                await Promise.all((await readdir(packagesPath)).map(async packageFolder => {
+                    const packageNodeModulesPath = path.join(packagesPath, packageFolder, nodeModules);
+                    if (await exists(packageNodeModulesPath)) {
+                        packages.push(packageFolder);
+                    }
+                }));
+                if (packages.length) {
+                    const selected = await vscode.window.showQuickPick(
+                        [
+                            { label: workspaceFolder.name, name: workspaceFolder.name},
+                            ...packages.map(packageName => ({ label: `${workspaceFolder.name}/${packageName}`, name: packageName }))
+                        ]
+                        , { placeHolder: 'Select package' }
+                    );
+                    if (!selected) {
+                        return;
+                    }
+
+                    if (selected !== workspaceFolder.name) {
+                        return {
+                            name: selected.name,
+                            path: path.join(packagesPath, selected.name)
+                        };
+                    }
+                }
+            }
+
+            return {
+                name: path.join(workspaceFolder.name, nodeModulesPath),
+                path: workspaceFolder.uri.fsPath
+            };
+        };
+
+        const getWorkspaceFolder = async () => {
+            if (vscode.workspace.workspaceFolders.length === 1) {
+                const folder = vscode.workspace.workspaceFolders[0];
+                return getPackageFolder(folder);
+            }
+
+            const selected = await vscode.window.showQuickPick(vscode.workspace.workspaceFolders.map(folder => ({
+                label: folder.name,
+                folder
+            })), {
+                placeHolder: 'Select workspace folder'
+            });
+
+            if (!selected) {
+                return;
+            }
+
+            return getPackageFolder(selected.folder);
+        };
+
         // Open last folder if there is one
         if (useLastFolder && lastFolder) {
             return searchPath(lastWorkspaceName, lastWorkspaceRoot, lastFolder);
@@ -85,24 +147,11 @@ exports.activate = context => {
             return showError('You must have a workspace opened.');
         }
 
-        // If in a multifolder workspace, prompt user to select which one to traverse.
-        if (vscode.workspace.workspaceFolders.length > 1) {
-            vscode.window.showQuickPick(vscode.workspace.workspaceFolders.map(folder => ({
-                label: folder.name,
-                folder
-            })), {
-                placeHolder: 'Select workspace folder'
-            })
-                .then(selected => {
-                    if (selected) {
-                        searchPath(selected.label, selected.folder.uri.fsPath, nodeModulesPath);
-                    }
-                });
-        } else {
-            // Otherwise, use the first one
-            const folder = vscode.workspace.workspaceFolders[0];
-            searchPath(folder.name, folder.uri.fsPath, nodeModulesPath);
-        }
+        getWorkspaceFolder().then(folder => {
+            if (folder) {
+                searchPath(folder.name, folder.path, nodeModulesPath);
+            }
+        });
     });
 
     context.subscriptions.push(searchNodeModules);
