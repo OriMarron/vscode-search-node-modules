7a4d76d7cc0d69d5f8b47f6254acb275bab38613
Ori Marron <orim@wix.com>
2019-05-06 14:24:55 +0300

    tidy up

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 const vscode = require('vscode');
 const fs = require('fs');
 const path = require('path');
-const { findModuleDirs } = require('./utils');
+const { findLernaPackageDirs } = require('./utils');
 
 var lastFolder = '';
 var lastWorkspaceName = '';
@@ -76,49 +76,54 @@ exports.activate = context => {
             });
         };
 
-        const getPackageFolder = async (workspaceFolder) => {
-            const packages = await findModuleDirs(workspaceFolder.uri.fsPath);
-            if (packages.length > 1) {
+        const getProjectFolder = async (workspaceFolder) => {
+            const packages = await findLernaPackageDirs(workspaceFolder.uri.fsPath);
+            // If in a lerna monorepo, prompt user to select which project to traverse
+            if (packages.length > 0) {
                 const selected = await vscode.window.showQuickPick(
-                    packages.map(packageName => ({ label: path.join(workspaceFolder.name, packageName), name: packageName }))
-                    , { placeHolder: 'Select package' }
+                    [
+                        { label: workspaceFolder.name, packageDir: '' },
+                        ...packages.map(packageDir => ({ label: path.join(workspaceFolder.name, packageDir), packageDir }))
+                    ]
+                    , { placeHolder: 'Select Project' }
                 );
                 if (!selected) {
                     return;
                 }
 
-                if (selected !== workspaceFolder.name) {
-                    return {
-                        name: selected.label,
-                        path: path.join(workspaceFolder.uri.fsPath, selected.name)
-                    };
-                }
+                return {
+                    name: selected.label,
+                    path: path.join(workspaceFolder.uri.fsPath, selected.packageDir)
+                };
             }
 
+            // Otherwise, use the root folder
             return {
-                name: path.join(workspaceFolder.name, nodeModulesPath),
+                name: workspaceFolder.name,
                 path: workspaceFolder.uri.fsPath
             };
         };
 
         const getWorkspaceFolder = async () => {
-            if (vscode.workspace.workspaceFolders.length === 1) {
-                const folder = vscode.workspace.workspaceFolders[0];
-                return folder;
+            // If in a multifolder workspace, prompt user to select which one to traverse.
+            if (vscode.workspace.workspaceFolders.length > 1) {
+                const selected = await vscode.window.showQuickPick(vscode.workspace.workspaceFolders.map(folder => ({
+                    label: folder.name,
+                    folder
+                })), {
+                    placeHolder: 'Select workspace folder'
+                });
+
+                if (!selected) {
+                    return;
+                }
+
+                return selected.folder;
             }
 
-            const selected = await vscode.window.showQuickPick(vscode.workspace.workspaceFolders.map(folder => ({
-                label: folder.name,
-                folder
-            })), {
-                placeHolder: 'Select workspace folder'
-            });
-
-            if (!selected) {
-                return;
-            }
-
-            return selected.folder;
+            // Otherwise, use the first one
+            const folder = vscode.workspace.workspaceFolders[0];
+            return folder;
         };
 
         // Open last folder if there is one
@@ -131,7 +136,7 @@ exports.activate = context => {
             return showError('You must have a workspace opened.');
         }
 
-        getWorkspaceFolder().then(folder => folder && getPackageFolder(folder)).then(folder => {
+        getWorkspaceFolder().then(folder => folder && getProjectFolder(folder)).then(folder => {
             if (folder) {
                 searchPath(folder.name, folder.path, nodeModulesPath);
             }
--- utils.js
+++ utils.js
@@ -7,11 +7,10 @@ const glob = util.promisify(require('glob'));
 const exists = util.promisify(fs.exists);
 const readFile = util.promisify(fs.readFile);
 
-const LERNA_CONFIG_FILE = 'lerna.json';
-
 const flat = arrays => [].concat.apply([], arrays);
 
-const findModuleDirs = async root => {
+const findLernaPackageDirs = async root => {
+    const LERNA_CONFIG_FILE = 'lerna.json';
     if (await exists(path.join(root, LERNA_CONFIG_FILE))) {
         const data = await readFile(path.join(root, LERNA_CONFIG_FILE));
 
@@ -22,14 +21,14 @@ const findModuleDirs = async root => {
             await Promise.all(
                 packagesConfig.map(pattern => {
                     if (pattern.includes('**')) return [];
-                    return glob(path.join(root, pattern, '/package.json'));
+                    return glob(path.join(pattern, '/package.json'), { cwd: root });
                 })
             )
         );
 
-        return [ '', ...matches.map(match => path.relative(root, match)) ];
+        return [ ...matches.map(match => path.join(match, '..')) ];
     }
-    return [ '' ];
+    return [];
 };
 
-module.exports = { findModuleDirs };
+module.exports = { findLernaPackageDirs };
